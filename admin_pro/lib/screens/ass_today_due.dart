74c940d8e494c78bee784f4974022e8d294ef702
Piyush Nirala <nirala.raghav7@gmail.com>
2020-12-27 21:03:13 +0530

    Assignment Today & Timed Today added

---
+++ admin_pro/lib/screens/ass_today_due.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:admin_pro/theme/colors/light_colors.dart';
+import 'package:admin_pro/widgets/data.dart';
+import 'package:expandable/expandable.dart';
+import 'package:admin_pro/widgets/task_container.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AssDueToday extends StatefulWidget {
+  @override
+  _AssDueTodayState createState() => _AssDueTodayState();
+}
+
+class _AssDueTodayState extends State<AssDueToday> {
+
+  Text subheading(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+          color: LightColors.kPalePink,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 1.2),
+    );
+  }
+
+  Widget _buildListItem(
+      BuildContext context,
+      DocumentSnapshot doc,
+      ) {
+    return TaskContainer(
+      title: doc['student'],
+      subtitle: "Due " + doc['due_date'].toDate().toString(),
+      boxColor: LightColors.kPalePink,
+      price: doc['price'],
+      tutor: doc['tutor'],
+      id: doc.id,
+      // a:assignments[index]
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference assgs =
+    FirebaseFirestore.instance.collection('assignments');
+    DateTime n = DateTime.now();
+    DateTime n1 = DateTime.parse("${n.year}-${n.month}-${n.day} 00:00:00Z");
+
+    List<DocumentSnapshot> dc = new List();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Expanded(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 15.0),
+                    Text(
+                      "Due Today",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0,
+                      ),
+                    ),
+                    SizedBox(height: 15.0),
+                    StreamBuilder(
+                      stream: assgs.snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (!snapshot.hasData) return Text("Loading.......");
+
+                        for (DocumentSnapshot doc
+                        in snapshot.data.documents) {
+                          DateTime d = doc['due_date'].toDate();
+                          DateTime t = DateTime.now();
+                          if (d.day == t.day &&
+                              d.month == t.month &&
+                              d.year == t.year) {
+                            dc.add(doc);
+                          }
+                        }
+
+                        return ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: dc.length,
+                          itemBuilder: (BuildContext context, int index) {
+
+                            return _buildListItem(
+                              context,
+                              dc[index],
+                            );
+                          },
+                          separatorBuilder:
+                              (BuildContext context, int index) =>
+                          const Divider(),
+                          physics: const NeverScrollableScrollPhysics(),
+                        );
+                      },
+                    ),
+
+                    Divider(
+                      color: Colors.black12,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

81ede44fd31640caebe6dd9a901a0b196c0f721c
abhay <abhay.katheria1998@gmail.com>
2021-01-07 00:50:05 +0530

    sorted

--- admin_pro/lib/screens/in_progress.dart
+++ admin_pro/lib/screens/in_progress.dart
@@ -46,7 +46,7 @@ class InProgress extends StatefulWidget {
   _InProgressState createState() => _InProgressState();
 }
 
-class _InProgressState extends State<InProgress> {
+class _InProgressState extends State<InProgress>  with SingleTickerProviderStateMixin {
 
 
   Widget _buildListItem(
@@ -65,10 +65,93 @@ class _InProgressState extends State<InProgress> {
     );
   }
 
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    CollectionReference assgs =
+        FirebaseFirestore.instance.collection('assignments');
 
+
+    Widget getData(String s){
+
+      return Container(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Expanded(
+                              child: Column(
+                  children: <Widget>[
+                    
+                    SizedBox(height: 15.0),
+                    StreamBuilder(
+                        stream: assgs.where('satus', isEqualTo: "ongoing").orderBy(s, descending: true).snapshots(),
+                        builder: (BuildContext context, AsyncSnapshot snapshot) {
+                          if (!snapshot.hasData) return Text("Loading.......");
+                          return ListView.separated(
+                            shrinkWrap: true,
+                            itemCount: snapshot.data.documents.length,
+                            itemBuilder: (BuildContext context, int index) {
+
+                              return _buildListItem(
+                                context,
+                                snapshot.data.documents[index],
+                              );
+                            },
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                            physics: const NeverScrollableScrollPhysics(),
+                          );
+                        },
+                      ),
+                    
+                    Divider(
+                      color: Colors.black12,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("In progress"),
+        backgroundColor: Colors.blueGrey,
+        bottom: TabBar(
+          unselectedLabelColor: Colors.white,
+          labelColor: Colors.white,
+          tabs: [
+            Tab(
+              child: Text("sorted by due_date"),
+            ),
+            Tab(
+              child: Text("sorted by assg_date"),
+            ),
+            
+          ],
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          indicatorSize: TabBarIndicatorSize.tab,
+        ),
+        bottomOpacity: 1,
+      ),
+      body: TabBarView(
+        children: [
+          getData("due_date"),
+          getData("assigned_date"),
+        ],
+        controller: _tabController,
+      ),
     );
   }
 }
